317fa2e485a0937f07b38b9f2f51232422d1caca
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-10-03 18:12:12 +0300

    initial commit

--- user_api/models.py
+++ user_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django import forms
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+	image = models.ImageField(blank=True, null=True)
+	telephone = models.IntegerField(default=0, min_length=9)
+	address = models.CharField(max_length=100)
+	credit = models.IntegerField(default=0)
+
+
---
+++ user_api/serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Profile
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'first_name', 'last_name', 'access']
+
+    def create(self, validated_data):
+        first_name = validated_data['first_name']
+		last_name = validated_data['last_name']
+        username = validated_data['username']
+        password = validated_data['password']
+        new_user = User(username=username)
+        new_user.set_password(password)
+        new_user.save()
+		validated_data["access"] = get_token(new_user)
+        return validated_data
+
+class ProfileSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Profile
+		fields = ['user', 'image', 'credit', 'telephone', 'address']
